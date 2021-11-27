d3a6ba91868a3965cd6a23070a9c242e1cc8ee56
keunlee <kelee@redhat.com>
2021-11-26 21:38:13 -0600

    added delays and updated clean script

--- ocp/cleanup.sh
+++ ocp/cleanup.sh
@@ -1,8 +1,15 @@
-oc -n kafka delete "$(oc api-resources --namespaced=true --verbs=delete -o name | tr "\n" "," | sed -e 's/,$//')" --all
-oc -n keycloak delete "$(oc api-resources --namespaced=true --verbs=delete -o name | tr "\n" "," | sed -e 's/,$//')" --all
+oc -n kafka delete kafka --all
+oc -n kafka delete kafkatopics --all
+oc -n kafka delete subscriptions amq-streams
+oc -n kafka delete operatorgroups kafka-operatorgroup
+oc delete project kafka
+
+oc -n keycloak delete keycloakrealms --all
+oc -n keycloak delete keycloak --all
+oc -n keycloak delete subscriptions rhsso-operator
+oc -n keycloak delete operatorgroups keycloak-rhsso-operatorgroup
+oc delete project keycloak
 
 oc delete project openldap
-oc delete project keycloak
-oc delete project kafka
 
 rm -rf bootstrap/roles/ocp4-install-rh-amqstreams/files/out
